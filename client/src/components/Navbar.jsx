71bd8f2e31ce2c1faddb1c9f25cefdaa6e637969
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-07-03 10:17:42 +0530

    update navbar for mobile responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,20 +8,20 @@ const Navbar = () => {
   const { user } = useUser();
 
   return (
-    <div className='shadow py-4'>
-      <div className='container px-4 2xl:px-20 mx-auto flex justify-between'>
+    <div className='py-4 shadow'>
+      <div className='container flex justify-between px-4 mx-auto 2xl:px-20'>
         <img src={assets.logo} alt="" />
         {
           user 
           ? <div className='flex items-center gap-3'>
             <Link to={'/applications'}>Applied Jobs</Link>
             <p>|</p>
-            <p>Hi, {user.firstName+" "+user.lastName}</p>
+            <p className='max-sm:hidden'>Hi, {user.firstName+" "+user.lastName}</p>
             <UserButton />
           </div> :
             <div className='flex gap-4 max-sm:text-sm'>
               <button className='text-gray-600'>Recruiter Login</button>
-              <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+              <button onClick={e => openSignIn()} className='px-6 py-2 text-white bg-blue-600 rounded-full sm:px-9'>Login</button>
             </div>
         }
       </div>
